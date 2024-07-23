54dd98a1e29674e9ecc5ec7b4fe340321497e149
Al-Muhajeer <168719514+Al-Muhajeer@users.noreply.github.com>
2024-07-23 14:26:16 +0300

    Update right_triangle.rb

--- Part2/right_triangle.rb
+++ Part2/right_triangle.rb
@@ -8,24 +8,23 @@ print 'Enter third:'
 c = gets.to_f**2
 
 if a == b && b == c
-	puts "Треугольник равнобедренный и равносторонний, но не прямоугольный"
+  puts "Треугольник равнобедренный и равносторонний, но не прямоугольный"
 else
-	if a > b && a > c
+  if a > b && a > c
     hypotenuse = a
     first = b
     second = c
   elsif b > a && b > c
-   	hypotenuse = b
+    hypotenuse = b
     first = a
     second = c
   elsif c > a && c > b
-  	hypotenuse = c
-   	first = a
+    hypotenuse = c
+    first = a
     second = b
   end
   if hypotenuse == first + second
-		puts 'Треугольник прямоугольный'
-		puts 'и равнобедренный' if a == b || b == c || c == a 
-	end
+    puts 'Треугольник прямоугольный'
+    puts 'и равнобедренный' if a == b || b == c || c == a 
+  end
 end
-
