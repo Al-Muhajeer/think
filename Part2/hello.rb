9188e703e2af966400cea65353d290cc5f8fc6e1
Al-Muhajeer <almuhajeerbaliev@gmail.com>
2024-07-23 15:21:51 +0300

    Fix to i

--- Part2/hello.rb
+++ Part2/hello.rb
@@ -2,6 +2,6 @@ puts 'Как тебя зовут? '
 name = gets.chomp
 
 puts 'В каком году ты родился?'
-year = gets.chomp
+year = gets.chomp.to_i
 
-puts "#{name}, привет! Тебе примерно #{2024 - year.to_i}"
+puts "#{name}, привет! Тебе примерно #{2024 - year}"
